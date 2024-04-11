d276b0fce66fdfa24a7743e985018280018770b6
khayrul200822 <khayrulislam200822@gmail.com>
2024-04-12 00:49:20 +0600

    update header mockup

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+//swiper script
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // breakpoints: {
+    //   640: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //   },
+    //   768: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //   },
+    //   1024: {
+    //     slidesPerView: 5,
+    //     spaceBetween: 50,
+    //   },
+    // },
+  });
